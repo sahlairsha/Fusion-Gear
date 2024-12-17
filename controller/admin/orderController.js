92c4a1fd7444d461f810b0239afc67992600fe69
sahlairsha <fsahla370@gmail.com>
2024-12-17 12:30:48 +0530

    Resolved problem in orders in admin side

--- controller/admin/orderController.js
+++ controller/admin/orderController.js
@@ -2,31 +2,49 @@
 
 const Order = require('../../models/orderSchema');
 
-
-const getOrders = async(req,res)=>{
+const getOrders = async (req, res) => {
     try {
-
         let page = req.query.page || 1;
-        let limit= 5
-        
+        let limit = 5;
+
         const orders = await Order.find({})
-         .limit(limit * 1)
+            .limit(limit * 1)
             .skip((page - 1) * limit)
-            .populate('user_id', 'full_name') 
+            .populate('user_id', 'full_name')
             .populate('products.product_id')
+            .populate('shippingAddress.addressDocId')
             .exec();
-          const count = await Order.find().countDocuments()
-    
+
+        const count = await Order.find().countDocuments();
+
+        // Accessing specific shipping address for each order
+        const ordersWithAddresses = orders.map(order => {
+            if (order.shippingAddress && order.shippingAddress.addressDocId) {
+                // Safely access specific address if exists
+                const specificAddress = order.shippingAddress.addressDocId.address[order.shippingAddress.addressIndex];
+                return {
+                    ...order.toObject(),
+                    specificAddress: specificAddress || null
+                };
+            } else {
+                return {
+                    ...order.toObject(),
+                    specificAddress: null
+                };
+            }
+        });
+
         res.render('orders', {
-            orders,
-            totalPages : Math.ceil(count/limit),
-            currentPage:page,
-        })
+            orders: ordersWithAddresses,
+            totalPages: Math.ceil(count / limit),
+            currentPage: page
+        });
     } catch (error) {
         console.error(error);
         res.status(500).send('Server Error');
     }
-}
+};
+
 const changeOrderStatus = async (req, res) => {
     try {
         const { id } = req.params;
